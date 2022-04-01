75e75f38aa270316fa1b96987a623c9491f80e34
INecati <83501943+INecati@users.noreply.github.com>
2022-04-01 04:34:59 +0300

    Ball select buttons changed

--- Assets/Entities/Entities.cs
+++ Assets/Entities/Entities.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Entities
 {
@@ -11,5 +14,12 @@ namespace Assets.Entities
         BasicBall = 0,
         BigBall = 1,
         ExplosiveBall = 2
+    }
+    [System.Serializable]
+    public struct BallSelectButton
+    {
+        public Button button;
+        public Image backgroundImage;
+        public TMP_Text text;
     }
 }
